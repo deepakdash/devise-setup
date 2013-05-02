3067035875b5b26d1bc7ee963f3ad0732753db58
deepak <deepakrip007@gmail.com>
2013-05-02 10:38:32 +0530

    added
    Signed-off-by: deepak <deepakrip007@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KamPagi::Application.config.secret_token = '281e621faca2a92e675305e4e2975a6b873bba6d1aaf4579f573ef260d1b76d30df4ff93171be7774a88bfb2bb7249a46a29854eab3a4bf93d4262013ab7c966'
